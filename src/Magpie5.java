b86eb97062918f883677a8fc507b5f4a2165f7a4
tmessall <messalltt@s.dcsdk12.org>
2018-11-14 08:09:29 -0700

    Add more responses to the array at the end.

--- src/Magpie5.java
+++ src/Magpie5.java
@@ -250,6 +250,7 @@ public class Magpie5 {
 	}
 
 	private String[] randomResponses = { "Interesting, tell me more", "Hmmm.",
-			"Do you really think so?", "You don't say." };
+			"Do you really think so?", "You don't say.", "Sounds like something you care about more than me",
+			"I totally understand.", "Obviously.", "Cool." };
 
 }
